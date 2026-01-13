c24cd170e2fb8f77c6396b9e184da43b5c3f135a
Sakshi more <moresakshi796@gmail.com>
2026-01-13 20:45:12 +0530

    Add files via upload

--- greater.py
+++ greater.py
@@ -1,4 +1,22 @@
 if 5 > 2:
-    print("5 is greater than two  ");
-    print("Happy new year to all!!!");
+    #print("5 is greater than two  ")
+    print("Happy new year to all!!!")
     print("Good moring")
+    #print('have a Good day')
+    print(1519)
+    print(5 * 9)
+    print(8 + 10)
+    #this is basic operation i do in first day of python
+    x = 2
+    y = "sakshi"
+    z = "DODO"
+    print(x)
+    print(y)
+    print(z)
+    a = str(2)
+    b = int(4)
+    c = float(3)
+    print(a)
+    print(b)
+    print(c)
+    
