66be49ef2f7d6000fc1642f6293977a4157d0256
Sakshi more <moresakshi796@gmail.com>
2026-01-14 21:04:05 +0530

    Add files via upload

---
+++ Light.py
@@ -0,0 +1,11 @@
+light = input("Light color : ")
+
+if(light == "red"):
+    print("Stop")
+elif(light == "Green"):
+    print("Go")
+elif(light == "Yellow"):
+    print("Look up")
+else:
+    print("Light is broken")    
+   
