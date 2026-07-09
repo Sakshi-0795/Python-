57b300e1c4387b3b1e38d79ee447d39cf6671905
Sakshi more <moresakshi796@gmail.com>
2026-07-09 16:21:27 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+num = int(input())
+temp = num
+rev = 0
+
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num //=10
+if temp == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")    
