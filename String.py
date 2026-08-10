9640d08a6a51525f6e7718c1eb5be138a44ee6be
Sakshi more <moresakshi796@gmail.com>
2026-08-10 14:02:18 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,12 @@
+name = "Sakshi more"
+garde = 'B'
+
+print(name.upper())
+print(name.find("m"))
+print(name.replace("Sakshi More" , "Akshara More"))
+print(name.replace("Sakshi","Akshara"))
+
+#cheak a presence
+print("S" in name)
+print("z" in name)
+
