39c025d73d2e1cdb4b4d7bed67b0589652de0ec5
Sakshi more <moresakshi796@gmail.com>
2026-08-10 14:01:49 +0530

    Add files via upload

---
+++ sub.py
@@ -0,0 +1,13 @@
+x = int(input("enter x : "))
+y = int(input("Enter y : "))
+
+sub = x - y
+
+print("sub",sub )
+
+m  = int(input("Enter m "))
+n  = int(input("Enter n "))
+
+mul = m * n
+
+print("mul  : " , mul)
