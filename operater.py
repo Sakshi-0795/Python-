5fc4bf442d1fcd173f0147b6159194eea8988a44
Sakshi more <moresakshi796@gmail.com>
2026-08-11 15:47:08 +0530

    Add files via upload

---
+++ operater.py
@@ -0,0 +1,20 @@
+#Arthmetic operater
+#priority of opreations in programming is like --() , > , * , / , < ,+,- 
+ans = (2 + 5) * 3
+print(ans)
+
+#comaprision operater:-
+#>,<,>=,<=,==,!=
+print(1 > 3)
+
+#Logical operater:- 
+#OR , AND , NOT
+
+stt1 = 3 > 5
+stt2 = 3 > 2
+
+print(stt1 or stt2)
+print(stt1 and stt2)
+print(not (3 > 5))
+
+
