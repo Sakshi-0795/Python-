a110cb895a7bc2a5a1b9a1376115534cf5ded1b9
Sakshi more <moresakshi796@gmail.com>
2026-08-11 15:57:39 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,11 @@
+# Conditionals Statements:-
+age  = 10
+
+if age>=18:
+    print("You are a adult")
+    print("You can drive / vote")
+elif age < 18:
+    print("You can't  vote or drive")
+
+print("End of the code")    
+
