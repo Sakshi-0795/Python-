b9834e42519b5081cc4b2430203d016e74fad940
Sakshi more <moresakshi796@gmail.com>
2026-08-13 15:00:41 +0530

    Add files via upload

---
+++ for_Loop.py
@@ -0,0 +1,14 @@
+#for Loop
+#0 to 5
+for i in range(5):
+    print(i)
+
+print("***********")
+#1 to 5
+for i in range(1 , 6):
+    print(i)
+
+print("*************")
+for i in range (1 , 20):
+    if i % 2 == 0:
+      print(i)
