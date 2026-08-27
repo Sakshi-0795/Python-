af8475cbe2c7b542bb15017bf8768202b8e6749e
Sakshi more <moresakshi796@gmail.com>
2026-08-27 06:03:59 +0530

    Add files via upload

---
+++ Practiceque1.py
@@ -0,0 +1,53 @@
+#Print "Hello Python":-
+print("Hello Python!!!")
+
+#Create a variable called name, store your name in it, and print it.
+name = "Sakshi"
+print(name)
+
+#Create a variable called name, store your name in it, and print it.
+a = 10
+b = 20
+c = a + b
+print(c)
+
+#Create three variables name, age, and city, and print them in one line
+pname = "Sakshi More"
+page = 20
+pcity = "Sangli"
+print(pname , page , pcity)
+
+#Store two numbers and print their subtraction
+m = 10
+n = 5
+k = m - n
+print("Subtraction is " , k)
+
+#Take length = 10 and width = 5 and calculate the area of a rectangle
+length = 10
+width = 5 
+area = length * width
+print("Area of Rectangle is ", area)
+
+#Take price = 100 and quantity = 5 and calculate the total price
+price  = 100
+quantity = 5
+total  = 100 * 5
+print(total)
+
+#Store your name and college in separate variables and print them in a sentence.
+my_name = "Sakshi More"
+college_name = "Krishna institue wathar"
+print(my_name , college_name)
+
+#Take two numbers and print their addition, subtraction, and multiplication
+x = 40
+y = 50
+print(x + y)
+print(x - y)
+print(x * y)
+
+#Create first_name and last_name variables and print the full name.
+first_name = "Sakshi"
+last_name = "More"
+print(first_name ,last_name)
