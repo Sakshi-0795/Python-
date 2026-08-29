e6840274b479d3b14ae6b9da894760e011a845da
Sakshi more <moresakshi796@gmail.com>
2026-08-29 19:43:21 +0530

    Add files via upload

---
+++ Practiceque2.py
@@ -0,0 +1,47 @@
+#Create an integer variable and print its data type.
+age = 20 
+print(type(age))
+
+#Create a float variable and print its data type.
+marks = 90.99
+print(type(marks))
+
+#Create a string variable and print its data type.
+name = "Sakshi"
+print(type(name))
+
+#Create a boolean variable and print its data type.
+if_student = True
+print(type(if_student))
+
+#Check the data type of age = 22.
+age = 22
+print(type(age))
+
+#Check the data type of price = 99.50.
+price = 99.50
+print(type(price))
+
+#Check the data type of name = "Sakshi".
+name = "Sakshi"
+print(type(name))
+
+#Check the data type of is_student = True.
+if_married = True
+print(type(if_married))
+
+person = 2
+percentage = 99.99
+status = "Rich"
+is_adult = True
+print(type(person))
+print(type(percentage))
+print(type(status))
+print(type(is_adult))
+
+#Take name and age as input from the user and print their data types.
+pname = (input("Enter a name"))
+page = int(input("Enter a age"))
+
+print(type(pname))
+print(type(page))
