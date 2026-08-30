5f460a0da7d034bbc1eec240f86e4aad3c94313b
Sakshi more <moresakshi796@gmail.com>
2026-08-30 12:36:59 +0530

    Add files via upload

---
+++ practiceque6.py
@@ -0,0 +1,6 @@
+#Take three numbers from the user and calculate their sum.
+a = int(input("Enter a number"))
+b = int(input("Enter a number"))
+c = int(input("Enter a number"))
+print("sum of three number  is : " ,  a + b + c)
+
